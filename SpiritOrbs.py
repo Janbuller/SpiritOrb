de9ad51371c11c85a2155b0616230dacff3ffc67
Lasse Jensen <lasse@thegamersofpro.com>
2017-06-12 10:03:29 +0200

    Added SpiritOrbs.py

---
+++ SpiritOrbs.py
@@ -0,0 +1,22 @@
+import random
+
+# Print New Line
+def print_NL(Printing = []):
+    for i in Printing:
+        print(i)
+
+# Random From List
+def Random_FL(List = []):
+    a = random.randint(0, len(List) - 1)
+    return List[a]
+
+# Random Letters
+def Random_Letter(Amount = 1, Numbers = False, Letters_Used = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"):
+    a = ""
+    b = 0
+    if Numbers == True:
+        Letters_Used = Letters_Used + "12345678901234567890"
+    while Amount > b:
+        a = a + Letters_Used[random.randint(0, len(Letters_Used) - 1)]
+        b += 1
+    return a
